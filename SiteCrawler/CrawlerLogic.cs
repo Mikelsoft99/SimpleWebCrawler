c4723b4951415abc9a125846c8bca11aa2cde925
Michael Zimmerhofer <mikelsoft99@gmail.com>
2019-08-30 10:22:52 +0200

    Adding MetaInfo Option (#6)
    * adding options to include metaDatas
    * bug fix - improving output

--- SiteCrawler/CrawlerLogic.cs
+++ SiteCrawler/CrawlerLogic.cs
@@ -11,14 +11,16 @@ namespace SiteCrawler
     class CrawlerLogic
     {
         public string BaseDomain;
-        public bool ExtractLinks {get; set;}
+        public bool ExtractLinks { get; set; }
+        public bool IncludeMetaData { get; set; }
         public string ClassSelector { get; set; }
 
         protected List<string> PagesToCrawl = new List<string>();
         public List<string> PagesCraweled = new List<string>();
         public Dictionary<string, List<string>> PagesCrawledLinks = new Dictionary<string, List<string>>();
+        public List<Models.MetaData> MetaData = new List<Models.MetaData>();
         protected List<string> PagesFailed = new List<string>();
-        
+
 
         protected PoliteWebCrawler Crawler;
         protected FileWriter FileWriter;
@@ -41,8 +43,9 @@ namespace SiteCrawler
         /// define the option for extracing the links too
         /// </summary>
         /// <param name="s"></param>
-        public void SetOptionExtractLink(string s) {
-            ExtractLinks =  s == "y" ? true : false;
+        public void SetOptionExtractLink(string s)
+        {
+            ExtractLinks = s == "y" ? true : false;
         }
 
         /// <summary>
@@ -54,6 +57,16 @@ namespace SiteCrawler
         {
             ClassSelector = c;
         }
+
+        /// <summary>
+        /// define the option for including the meta data
+        /// </summary>
+        /// <param name="s"></param>
+        public void SetOptionIncludeMetaData(string s)
+        {
+            IncludeMetaData = s == "y" ? true : false;
+        }
+
 
         private void SetBaseDomain(string url)
         {
@@ -144,11 +157,81 @@ namespace SiteCrawler
             else
             {
                 Console.WriteLine("Crawl OK: {0}", page.Uri.ToString());
-                Console.WriteLine();
                 string currentURL = page.Uri.ToString();
                 AddToCrawledPages(currentURL);
 
-                if(ExtractLinks) {
+                if(IncludeMetaData)
+                {
+                    Console.Write("Extracting meta: ");
+                    // identiyfying non page link
+                    if (IsPageUrl(currentURL))
+                    {
+
+                        AngleSharp.Dom.Html.IHtmlDocument htmlPage = page.AngleSharpHtmlDocument;
+
+                        // output
+                        Models.MetaData metaDataDTO = new Models.MetaData();
+                        metaDataDTO.Url = currentURL;
+
+                        // options
+                        string titleSelector = "title";
+                        string metaSelector = "meta";
+                        string[] metaAttributes = new[] { "description", "keywords" };
+
+                        // title
+                        AngleSharp.Dom.IElement titleElement = htmlPage.QuerySelector(titleSelector);
+                        metaDataDTO.Title = (titleElement != null) ? titleElement.TextContent : "";
+                        Console.Write("title ");
+
+                        // description
+                        AngleSharp.Dom.IHtmlCollection<AngleSharp.Dom.IElement> metaElements = htmlPage.QuerySelectorAll(metaSelector);
+                        foreach (var item in metaElements)
+                        {
+                            if (item.HasAttribute("name"))
+                            {
+                                if (item.Attributes["name"].Value == metaAttributes[0])
+                                {
+                                    try
+                                    {
+                                        metaDataDTO.Description = item.Attributes["content"].Value;
+                                    }
+                                    catch (Exception)
+                                    {
+
+                                        throw;
+                                    }
+                                    Console.Write(metaAttributes[0] + " ");
+                                }
+                                if (item.Attributes["name"].Value == metaAttributes[1])
+                                {
+                                    try
+                                    {
+                                        metaDataDTO.Keywords = item.Attributes["content"].Value;
+                                    }
+                                    catch (Exception)
+                                    {
+
+                                        throw;
+                                    }
+                                    Console.Write(metaAttributes[1] + " ");
+                                }
+                            }
+                        }
+
+                        // add
+                        MetaData.Add(metaDataDTO);
+
+                        // output to console
+                        Console.WriteLine("     OK");
+                    }
+                    else
+                    {
+                        Console.WriteLine("     NO VALID PAGE");
+                    }
+                }
+
+                if (ExtractLinks)
+                {
 
 
                     AngleSharp.Dom.Html.IHtmlDocument htmlPage = page.AngleSharpHtmlDocument;
@@ -182,7 +265,7 @@ namespace SiteCrawler
 
                             if (!linkValue.StartsWith("http"))
                             {
-                                linkValue = baseUri+"/"+linkValue;
+                                linkValue = baseUri + "/" + linkValue;
                             }
                             return linkValue;
                         }
@@ -195,6 +278,9 @@ namespace SiteCrawler
                     // store in dictionary
                     this.PagesCrawledLinks.Add(currentURL, linksFound);
                 }
+
+                // new line
+                Console.WriteLine();
             }
 
         }
@@ -207,6 +293,22 @@ namespace SiteCrawler
         {
             PageToCrawl page = e.PageToCrawl;
             Console.WriteLine("Disallowed: {0}", page.Uri.ToString());
+        }
+
+
+        /* 
+         * helpers
+         */
+
+        public bool IsPageUrl(string url)
+        {
+            string[] nonValidEndings = new[] { ".jpg", ".png", ".pdf", ".gif", ".ttf" };
+            foreach (string e in nonValidEndings)
+            {
+                if (url.EndsWith(e)) return false;
+            }
+
+            return true;
         }
     }
 }
---
+++ SiteCrawler/Models/MetaData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteCrawler.Models
+{
+    public class MetaData
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Keywords { get; set; }
+        public string Url { get; set; }
+
+        public int DeepLevel
+        {
+            get
+            {
+                if(!string.IsNullOrEmpty(Title))
+                {
+                    return Title.ToCharArray().Count(x => x == '/');
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+}
